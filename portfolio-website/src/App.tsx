c894641367b4ef5420e22a9e53884450a8d1ecbf
Dylan-Dotti <invocation42@yahoo.com>
2021-12-25 17:29:26 -0500

    Added initial files

---
+++ portfolio-website/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="title-group">
+          <h1>Dylan Dotti</h1>
+        </div>
+        <nav>
+          <ul>
+            <li>About</li>
+            <li>Education</li>
+            <li>Employment</li>
+            <li>Projects</li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default App;
