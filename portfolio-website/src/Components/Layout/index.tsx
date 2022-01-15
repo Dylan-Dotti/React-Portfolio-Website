e3e3deaa48086876ba19c4fe1d02b09dc662e758
Dylan Dotti <invocation42@yahoo.com>
2022-01-15 15:53:01 -0500

    Create Layout component

---
+++ portfolio-website/src/Components/Layout/index.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+export default class Layout extends React.Component {
+  render(): ReactNode {
+    return (
+      <>
+        <div className="header">
+          <div className="title-group">
+            <h1>Dylan Dotti</h1>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">About</Link>
+              </li>
+              <li>
+                <Link to="/education">Education</Link>
+              </li>
+              <li>
+                <Link to="/employment">Employment</Link>
+              </li>
+              <li>
+                <Link to="/projects">Projects</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Outlet />
+      </>
+    );
+  }
+}
