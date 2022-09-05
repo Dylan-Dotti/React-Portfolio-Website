0b0a6df1455cb9c477843f1795ad3d8660b472a3
Dylan-Dotti <32313708+Dylan-Dotti@users.noreply.github.com>
2022-09-04 22:51:30 -0400

    Added Deviant cover photo

--- src/Components/MainHeader/index.tsx
+++ src/Components/MainHeader/index.tsx
@@ -7,24 +7,16 @@ export default class MainHeader extends React.Component {
     public render(): React.ReactNode {
         return (
             <div className="main-header">
-                <div className="title-group mx-3">
-                    <h1>Dylan Dotti</h1>
-                    Software Developer
+                <div className="title-group">
+                    <h1 className="name">Dylan Dotti</h1>
+                    <span className="subtitle">Software Developer</span>
                 </div>
                 <nav>
                     <ul className="nav-list">
-                        <li>
-                            <Link to="/">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/education">Education</Link>
-                        </li>
-                        <li>
-                            <Link to="/employment">Employment</Link>
-                        </li>
-                        <li>
-                            <Link to="/projects">Projects</Link>
-                        </li>
+                        <li><Link to="/">About</Link></li>
+                        <li><Link to="/education">Education</Link></li>
+                        <li><Link to="/employment">Employment</Link></li>
+                        <li><Link to="/projects">Projects</Link></li>
                     </ul>
                 </nav>
             </div>
