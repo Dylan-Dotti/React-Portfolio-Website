70c33900f0cff8c3ea951efac7c70125b775d429
Dylan Dotti <invocation42@yahoo.com>
2022-09-11 20:45:45 -0400

    Added header image

--- src/Components/MainHeader/index.tsx
+++ src/Components/MainHeader/index.tsx
@@ -6,20 +6,25 @@ export default class MainHeader extends React.Component {
 
     public render(): React.ReactNode {
         return (
-            <div className="main-header">
-                <div className="title-group">
-                    <h1 className="name">Dylan Dotti</h1>
-                    <span className="subtitle">Software Developer</span>
+            <>
+                <div className="crop">
+                    <img src="Img\Deviant\deviant_action.PNG" alt="" id="header-img" />
                 </div>
-                <nav>
-                    <ul className="nav-list">
-                        <li><Link to="/">About</Link></li>
-                        <li><Link to="/education">Education</Link></li>
-                        <li><Link to="/employment">Employment</Link></li>
-                        <li><Link to="/projects">Projects</Link></li>
-                    </ul>
-                </nav>
-            </div>
+                <div className="main-header">
+                    <div className="title-group">
+                        <h1 className="name">Dylan Dotti</h1>
+                        <span className="subtitle">Software Developer</span>
+                    </div>
+                    <nav>
+                        <ul className="nav-list">
+                            <li><Link to="/">About</Link></li>
+                            <li><Link to="/education">Education</Link></li>
+                            <li><Link to="/employment">Employment</Link></li>
+                            <li><Link to="/projects">Projects</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+            </>
         );
     }
 }
