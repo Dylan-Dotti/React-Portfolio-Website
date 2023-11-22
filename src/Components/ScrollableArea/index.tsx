a2ad1dff72fcc28431324da6c6ac1b562a5a8f45
Dylan Dotti <invocation42@yahoo.com>
2023-11-22 00:26:31 -0500

    Add overflow props

--- src/Components/ScrollableArea/index.tsx
+++ src/Components/ScrollableArea/index.tsx
@@ -6,12 +6,20 @@ interface IScrollableAreaProps {
     children         : React.ReactNode
     className?       : string
     contentClassName?: string
+    overflowX?       : 'visible' | 'hidden' | 'clip' | 'scroll' | 'auto'
+    overflowY?       : 'visible' | 'hidden' | 'clip' | 'scroll' | 'auto'
 }
 
 const ScrollableArea: React.FC<IScrollableAreaProps> = (props) => {
     return (
         <div className={classNames('scrollable-area', props.className)}>
-            <div className={classNames('scrollable-content', props.contentClassName)}>
+            <div
+                className={classNames('scrollable-content', props.contentClassName)}
+                style={{
+                    overflowX: props.overflowX || 'auto',
+                    overflowY: props.overflowY || 'auto',
+                }}
+            >
                 {props.children}
             </div>
         </div>
