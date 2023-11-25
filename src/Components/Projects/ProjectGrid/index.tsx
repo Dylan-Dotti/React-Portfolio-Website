44be1065adcfe5de992432704d5fa0f872b83f8e
Dylan Dotti <invocation42@yahoo.com>
2023-11-24 20:06:37 -0500

    Wrap ProjectGridItem in ScrollFadeInSection

--- src/Components/Projects/ProjectGrid/index.tsx
+++ src/Components/Projects/ProjectGrid/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import ProjectGridItem from './ProjectGridItem';
 import { IProjectData } from '../../../Interfaces/IProjectData';
 import './project-grid.scss';
+import ScrollFadeInSection from '../../ScrollFadeInSection';
 
 interface IProjectGridProps {
     projects: IProjectData[]
@@ -16,10 +17,11 @@ const ProjectGrid: React.FC<IProjectGridProps> = (props) => {
             {(!projects || projects.length === 0) && <p>No projects to display.</p>}
             {projects && projects.length > 0 && projects.map((project, index) => {
                 return (
-                    <ProjectGridItem
-                        key={project.id}
-                        project={project}
-                    />
+                    <ScrollFadeInSection key={project.id}>
+                        <ProjectGridItem
+                            project={project}
+                        />
+                    </ScrollFadeInSection>
                 );
             })}
         </div>
