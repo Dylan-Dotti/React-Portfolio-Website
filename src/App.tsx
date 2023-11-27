3d1296d6678c1d4c7b45e58f2e41410281046549
Dylan Dotti <invocation42@yahoo.com>
2023-11-27 01:29:43 -0500

    Create app-level context

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import "./App.scss";
 import "./Styles/panels.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -7,19 +8,25 @@ import Employment from "./Components/Employment";
 import Layout from "./Components/Layout";
 import Projects from "./Components/Projects";
 
+export const AppConstantsContext = React.createContext({
+  fadeInTransitionDurationMS: 0,
+});
+
 export default function App() {
   return (
     <div id="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<About />}></Route>
-            <Route path="/education" element={<Education />}></Route>
-            <Route path="/employment" element={<Employment />}></Route>
-            <Route path="/projects" element={<Projects />}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <AppConstantsContext.Provider value={{ fadeInTransitionDurationMS: 2000 }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<About />}></Route>
+              <Route path="/education" element={<Education />}></Route>
+              <Route path="/employment" element={<Employment />}></Route>
+              <Route path="/projects" element={<Projects />}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AppConstantsContext.Provider>
     </div>
   );
 }
