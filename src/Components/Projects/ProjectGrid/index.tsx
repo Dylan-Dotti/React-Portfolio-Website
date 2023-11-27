bb556f5bdcbddc30f67072bbabf20921336d3922
Dylan Dotti <invocation42@yahoo.com>
2023-11-27 01:37:08 -0500

    Parameterize ScrollFadeInSection instance

--- src/Components/Projects/ProjectGrid/index.tsx
+++ src/Components/Projects/ProjectGrid/index.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import ProjectGridItem from './ProjectGridItem';
 import { IProjectData } from '../../../Interfaces/IProjectData';
 import './project-grid.scss';
-import ScrollFadeInSection from '../../ScrollFadeInSection';
+import ScrollFadeInSection, { ScrollFadeInDirection } from '../../ScrollFadeInSection';
+import { AppConstantsContext } from '../../../App';
 
 interface IProjectGridProps {
     projects: IProjectData[]
@@ -12,12 +13,19 @@ const ProjectGrid: React.FC<IProjectGridProps> = (props) => {
     
     const { projects } = props;
 
+    const { fadeInTransitionDurationMS } = React.useContext(AppConstantsContext);
+
     return (
         <div className="project-grid">
             {(!projects || projects.length === 0) && <p>No projects to display.</p>}
             {projects && projects.length > 0 && projects.map((project, index) => {
                 return (
-                    <ScrollFadeInSection key={project.id}>
+                    <ScrollFadeInSection
+                        key={project.id}
+                        direction={index % 2 === 0 ? ScrollFadeInDirection.Left : ScrollFadeInDirection.Right}
+                        durationMS={fadeInTransitionDurationMS}
+                        shiftValue='30px'
+                    >
                         <ProjectGridItem
                             project={project}
                         />
