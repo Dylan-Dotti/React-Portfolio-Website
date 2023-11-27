965559fc5a779712a49af99c7cfa87766559a660
Dylan Dotti <invocation42@yahoo.com>
2023-11-27 01:31:41 -0500

    Make ScrollFadeInSection styles variable

--- src/Components/ScrollFadeInSection/index.tsx
+++ src/Components/ScrollFadeInSection/index.tsx
@@ -1,8 +1,27 @@
 import * as React from 'react';
 import './scroll-fade-in-section.scss';
 
+export enum ScrollFadeInDirection {
+    Up, Down, Left, Right
+}
+
+interface IScrollFadeInSectionProps {
+    children   : React.ReactNode
+    direction? : ScrollFadeInDirection
+    durationMS?: number
+    shiftValue?: string
+}
+
 // A wrapper component that fades in its children when they are scrolled into view.
-const ScrollFadeInSection: React.FC = (props) => {
+const ScrollFadeInSection: React.FC<IScrollFadeInSectionProps> = (props) => {
+
+    const { 
+        children,
+        direction = ScrollFadeInDirection.Up,
+        durationMS = 0, 
+        shiftValue = '0px'
+    } = props;
+
     const [isVisible, setVisible] = React.useState(false);
     const domRef = React.useRef<HTMLDivElement>(null);
 
@@ -24,9 +43,30 @@ const ScrollFadeInSection: React.FC = (props) => {
         
     }, []);
 
+    const style: React.CSSProperties = {
+        transition: `all ${durationMS}ms ease-out`,
+    };
+
+    switch (direction) {
+        case ScrollFadeInDirection.Up:
+            style.top = isVisible ? '0' : shiftValue;
+            break;
+        case ScrollFadeInDirection.Down:
+            style.bottom = isVisible ? '0' : shiftValue;
+            break;
+        case ScrollFadeInDirection.Left:
+            style.right = isVisible ? '0' : shiftValue;
+            break;
+        case ScrollFadeInDirection.Right:
+            style.left = isVisible ? '0' : shiftValue;
+            break;
+        default:
+            break;
+    }
+
     return (
-        <div className={`scroll-fade-in-section ${isVisible ? 'is-visible' : ''}`} ref={domRef}>
-            {props.children}
+        <div className={`scroll-fade-in-section ${isVisible ? 'is-visible' : ''}`} ref={domRef} style={style}>
+            {children}
         </div>
     );
 }
