9b5e6a721b9388dfc8cb9894bcc4528b912bd981
Dylan Dotti <invocation42@yahoo.com>
2023-11-30 21:37:08 -0500

    Education component first iteration

--- src/Components/Education/index.tsx
+++ src/Components/Education/index.tsx
@@ -1,10 +1,49 @@
-import React, { ReactNode } from "react";
+import React, { useContext } from 'react';
+import './education.scss';
+import ScrollFadeInSection, { ScrollFadeInDirection } from '../ScrollFadeInSection';
+import { AppConstantsContext } from '../../App';
+import CourseList from './CourseList';
 
-export default class Education extends React.Component {
+const Education: React.FC = () => {
+    const {fadeInDurationMS: fadeInTransitionDurationMS } = useContext(AppConstantsContext);
 
-    render(): ReactNode {
-        return (
-            <p>Education component works!</p>
-        );
-    }
+    return (
+        <div id="education">
+
+            <div className="header-group">
+                <ScrollFadeInSection durationMS={fadeInTransitionDurationMS}>
+                    <h1 className="text-center title">Education</h1>
+                    <hr className="half-centered"/>
+
+                    <img src="./Img/CNU/cnu_campus_1_cropped.jpg" alt="" className="main-image" />
+                </ScrollFadeInSection>
+                <div className="overview-section">    
+                    <ScrollFadeInSection durationMS={fadeInTransitionDurationMS} direction={ScrollFadeInDirection.Up}>
+                        <p>
+                            I obtained a Bachelor's degree in Computer Science from Christopher Newport University in 2019. Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis beatae vel harum sequi voluptatem rem laborum! Culpa qui aliquid veritatis aliquam dolor maiores asperiores fuga, fugit nulla ex, delectus numquam?
+                        </p>
+                    </ScrollFadeInSection>
+                </div>
+            </div>
+
+
+            <div className="courses-section">
+
+                <div className="title-group">
+                    <ScrollFadeInSection durationMS={fadeInTransitionDurationMS} direction={ScrollFadeInDirection.Up}>
+                        <h2 className="text-center title">Courses</h2>
+                        <p className="text-center">
+                            Below I've listed and summarized the most beneficial courses I took during my time at CNU.
+                        </p>
+                    </ScrollFadeInSection>
+                </div>
+
+                <CourseList />
+
+            </div>
+        </div>
+    );
+
 }
+
+export default Education;
