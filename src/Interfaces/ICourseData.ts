e8c8fe6d88b9b1749d92c64a248b3f7e2ed56f3d
Dylan Dotti <invocation42@yahoo.com>
2023-11-30 21:34:18 -0500

    Create ICourseData.ts

---
+++ src/Interfaces/ICourseData.ts
@@ -0,0 +1,10 @@
+import ILinkData from "./ILinkData"
+
+export default interface ICourseData {
+    id             : number
+    name           : string
+    overview?      : string
+    mainImgSrc?    : string
+    skills?        : string[]
+    links?         : ILinkData[]
+}
