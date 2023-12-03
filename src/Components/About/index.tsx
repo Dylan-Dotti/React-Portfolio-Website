8e01243c2fca1d5d63855fec637e63b3fd1e6fd8
Dylan Dotti <invocation42@yahoo.com>
2023-12-02 22:36:56 -0500

    Add Intro section placeholder to About Component

--- src/Components/About/index.tsx
+++ src/Components/About/index.tsx
@@ -1,10 +1,28 @@
-import React, { ReactNode } from "react";
+import * as React from "react";
+import "./about.scss";
+import ScrollFadeInSection from "../ScrollFadeInSection";
+import { AppConstantsContext } from "../../App";
 
-export default class About extends React.Component {
 
-    render(): ReactNode {
-        return (
-            <p>About component works!</p>
-        );
-    }
+const About: React.FC = () => {
+
+    const appConstants = React.useContext(AppConstantsContext);
+    const profileImg = "./img/Profile/IMG_0061_1.jpg";
+
+    return (
+        <div id="about">
+
+            <ScrollFadeInSection className="intro-section" durationMS={appConstants.fadeInDurationMS}>
+                <div className="profile-picture" style={{backgroundImage: `url(${profileImg})`}}></div>
+                <div className="intro-text">
+                    <h1>Lorem, ipsum dolor</h1>
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate</h4>
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, itaque</h4>
+                </div>
+            </ScrollFadeInSection>
+            
+        </div>
+    );
 }
+
+export default About;
