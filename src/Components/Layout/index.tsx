240699c6ad9f2460e499500e29848dfdb428795c
Dylan Dotti <invocation42@yahoo.com>
2023-12-04 01:06:42 -0500

    Scroll to top when switching components

--- src/Components/Layout/index.tsx
+++ src/Components/Layout/index.tsx
@@ -1,15 +1,22 @@
 import * as React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import MainHeader from "../MainHeader";
 import "./layout.scss";
 import ScrollableArea from "../ScrollableArea";
 import Footer from "../Footer";
 
 const Layout: React.FC = () => {
+    const { pathname } = useLocation();
+    const scrollContentRef = React.useRef<HTMLDivElement>(null);
+
+    React.useEffect(() => {
+        scrollContentRef.current?.scrollTo(0, 0);
+    }, [pathname]);
+
     return (
         <div id="page-layout">
             <MainHeader />
-            <ScrollableArea>
+            <ScrollableArea contentRef={scrollContentRef}>
                 <div id="page-content">
                     <Outlet />
                 </div>
