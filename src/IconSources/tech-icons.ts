81f0f6ebe00a3d7e9ebca1981104827a29e11dc1
Dylan Dotti <invocation42@yahoo.com>
2023-12-04 23:47:03 -0500

    Create tech-icons.ts

---
+++ src/IconSources/tech-icons.ts
@@ -0,0 +1,62 @@
+
+const techIconSrcs = {
+    htmlOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+    htmlOriginalWordmark: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg",
+    htmlPlainWordmark: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg",
+
+    cssOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+    cssPlainWordmark : "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg",
+    sassOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
+
+    jsOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    jsPlain: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+    
+    tsOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+    tsPlain: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg",
+
+    reactOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+
+    cSharpOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg",
+
+    pythonOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+
+    javaOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+
+    cPlusPlusOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg",
+
+    dotNetPlainWordmark: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dot-net/dot-net-plain-wordmark.svg",
+
+    angularJSOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg",
+
+    electronOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/electron/electron-original.svg",
+
+    bootstrapOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+
+    visualStudioPlain: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/visualstudio/visualstudio-plain.svg",
+    vsCodeOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+
+    nodeJSOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+
+    gitOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+
+    jiraOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jira/jira-original.svg",
+    jiraOriginalWordmark: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jira/jira-original-wordmark.svg",
+
+    npmOriginalWordmark: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg",
+
+    linuxOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg",
+
+    mongoDBPlainWordmark: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain-wordmark.svg",
+
+    unityOriginalWordmark: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/unity/unity-original-wordmark.svg",
+
+    blenderOriginal: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/blender/blender-original.svg",
+    blenderOriginalWordmark: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/blender/blender-original-wordmark.svg",
+
+};
+
+const getTechIcons = () => {
+    return { ...techIconSrcs }
+}
+
+export default getTechIcons;
