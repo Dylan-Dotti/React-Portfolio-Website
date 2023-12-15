7515672291ce36709d8e5304bd8e3f09b1c21aea
Dylan Dotti <invocation42@yahoo.com>
2023-12-15 00:59:22 -0500

    Move projects title

--- src/Components/Projects/index.tsx
+++ src/Components/Projects/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./projects.scss";
 import { Outlet } from "react-router-dom";
+import ScrollFadeInSection from "../ScrollFadeInSection";
+import { AppConstantsContext } from "../../App";
 
 const Projects: React.FC = () => {
+    const { fadeInDurationMS } = React.useContext(AppConstantsContext);
     
     return (
         <div id="projects">
+            <ScrollFadeInSection durationMS={fadeInDurationMS}>
+                <div className="content-container">
+                    <h2 className="title">Projects</h2>
+                    <hr className="half-centered"/>
+                </div>
+            </ScrollFadeInSection>
             <Outlet />
         </div>
     );
