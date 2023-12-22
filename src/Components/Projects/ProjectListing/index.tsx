999c578d7aec912990f42176c834636a4d57b907
Dylan Dotti <invocation42@yahoo.com>
2023-12-22 00:42:03 -0500

    Move divider into project-listing-item div

--- src/Components/Projects/ProjectListing/index.tsx
+++ src/Components/Projects/ProjectListing/index.tsx
@@ -47,65 +47,59 @@ const ProjectListing: React.FC<ProjectListingProps> = (props) => {
                 {(!props.projects || props.projects.length === 0) && <p>No projects to display.</p>}
                 {props.projects && props.projects.length > 0 && props.projects.map((project, index) => {
                     return (
-                        <div key={project.id}>
-                            <div className="project-listing-item">
+                            <div className="project-listing-item" key={project.id}>
 
-                                <div className="left-column">
-
-                                    <div className="project-section title-section">
-                                        <h2 className="project-name">{project.name}</h2>
-                                        <img src={project.imgSrc} alt="" className="main-image"/>
-
-                                        <div className="button-links">
-                                            {project.codeSrc && (
-                                                <a href={project.codeSrc} target="_blank" rel="noreferrer" className="btn btn-primary view-code-button">
-                                                    View Code
-                                                </a>
-                                            )}
-                                            <a href="#" className="btn btn-secondary">View Demos</a>
+                                <div className="item-content">
+                                    
+                                    <div className="left-column">
+                                        <div className="project-section title-section">
+                                            <h2 className="project-name">{project.name}</h2>
+                                            <img src={project.imgSrc} alt="" className="main-image"/>
+                                            <div className="button-links">
+                                                {project.codeSrc && (
+                                                    <a href={project.codeSrc} target="_blank" rel="noreferrer" className="btn btn-primary view-code-button">
+                                                        View Code
+                                                    </a>
+                                                )}
+                                                <a href="#" className="btn btn-secondary">View Demos</a>
+                                            </div>
+                                        </div>
+                                        <div className="project-section technologies-section">
+                                            <h3 className="section-header">Technologies Used</h3>
+                                            <ul className="technologies-list">
+                                                {/* TODO: add project technologies */}
+                                                {placeholderTechs.map((tech, index) => (
+                                                    <li key={index}>{tech}</li>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                        <div className="project-section skills-section">
+                                            <h3 className="section-header">Relevant Skills</h3>
+                                            <ul className="skills-list">
+                                                {placeholderSkills.map((skill, index) => (
+                                                    <li key={index}>{skill}</li>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    
+                                    <div className="right-column">
+                                        <div className="project-section">
+                                            <h3 className="section-header">Overview</h3>
+                                            <p>{project.description}</p>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur recusandae nesciunt officiis ex quis voluptatum veritatis sint facilis repudiandae corporis in illum molestiae, vel laboriosam rem iure iste odit deserunt!</p>
+                                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti est assumenda dolorum eveniet aut sapiente, sint illum iure commodi laudantium corrupti provident voluptatum doloribus quas a vero exercitationem nam veniam. Quaerat, iste porro? Vitae delectus iure, eius voluptatum tempore maiores.</p>
+                                        </div>
+                                        <div className="project-section">
+                                            <h3 className="section-header">Challenges</h3>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur recusandae nesciunt officiis ex quis voluptatum veritatis sint facilis repudiandae corporis in illum molestiae, vel laboriosam rem iure iste odit deserunt!</p>
                                         </div>
                                     </div>
 
-                                    <div className="project-section technologies-section">
-                                        <h3 className="section-header">Technologies Used</h3>
-                                        <ul className="technologies-list">
-                                            {/* TODO: add project technologies */}
-                                            {placeholderTechs.map((tech, index) => (
-                                                <li key={index}>{tech}</li>
-                                            ))}
-                                        </ul>
-                                    </div>
-
-                                    <div className="project-section skills-section">
-                                        <h3 className="section-header">Relevant Skills</h3>
-                                        <ul className="skills-list">
-                                            {placeholderSkills.map((skill, index) => (
-                                                <li key={index}>{skill}</li>
-                                            ))}
-                                        </ul>
-                                    </div>
-
-                                </div>
-                            
-                                
-                                <div className="right-column">
-
-                                    <div className="project-section">
-                                        <h3 className="section-header">Overview</h3>
-                                        <p>{project.description}</p>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur recusandae nesciunt officiis ex quis voluptatum veritatis sint facilis repudiandae corporis in illum molestiae, vel laboriosam rem iure iste odit deserunt!</p>
-                                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti est assumenda dolorum eveniet aut sapiente, sint illum iure commodi laudantium corrupti provident voluptatum doloribus quas a vero exercitationem nam veniam. Quaerat, iste porro? Vitae delectus iure, eius voluptatum tempore maiores.</p>
-                                    </div>
-
-                                    <div className="project-section">
-                                        <h3 className="section-header">Challenges</h3>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur recusandae nesciunt officiis ex quis voluptatum veritatis sint facilis repudiandae corporis in illum molestiae, vel laboriosam rem iure iste odit deserunt!</p>
-                                    </div>
                                 </div>
 
+                                {index < props.projects.length - 1 && <HrWithCircle className="softer" />}
                             </div>
-                            {index < props.projects.length - 1 && <HrWithCircle className="softer" />}
-                        </div>
                         
                     );
                 })}
